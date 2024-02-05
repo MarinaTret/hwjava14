3844a106d2410ec93fc943b768298c2f2f2403fa
MarinaTret <jiharka88@mail.ru>
2024-02-05 13:16:52 +0300

    Finish

---
+++ src/test/java/ru/netology/domain/ShopRepositoryTest.java
@@ -0,0 +1,36 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ShopRepositoryTest {
+
+    Product product1 = new Product(1, "Колбаса", 500);
+    Product product2 = new Product(2, "Сыр", 400);
+    Product product3 = new Product(3, "Оливки", 300);
+
+    @Test
+    public void successDel() {
+        ShopRepository repo = new ShopRepository();
+        repo.add(product1);
+        repo.add(product2);
+        repo.add(product3);
+
+        repo.removeById(3);
+
+        Product[] expected = {product1, product2};
+        Assertions.assertArrayEquals(expected, repo.findAll());
+    }
+
+    @Test
+    public void NotFound() {
+        ShopRepository repo = new ShopRepository();
+        repo.add(product1);
+        repo.add(product2);
+        repo.add(product3);
+
+        Assertions.assertThrows(NotFoundException.class, () -> {
+            repo.removeById(-10);
+        });
+    }
+}
